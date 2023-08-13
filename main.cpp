930b463fc60d0155c24bd08e30477231dc5df869
Alex Bogushenko <bogushenkoas@ukr.net>
2023-08-13 22:12:58 +0300

    11commit

--- main.cpp
+++ main.cpp
@@ -8,145 +8,143 @@ using namespace std;
 
 class Elf
 {
- public:
- string name;
-
+public:
+    string name;
 };
 
 class Branch
 {
-  public:
-  string name;
-  vector <Branch*> children;
-  vector <Elf*> elves;  
+public:
+    string name;
+    vector<Branch*> children;
+    Elf elves; // заменяем vector<Elf*> elves на Elf elves
 
-  Branch(string name)
-  {
-    this->name = name;
-  }
-
+    Branch(string name)
+    {
+        this->name = name;
+    }
 };
+
+
 
 int main()
 {
  srand(time(nullptr));
 
-    vector <Branch*> trees;
+    vector<Branch*> trees;
 
     // Создаем деревья и их ветви
-    for(int i = 0; i < 5; i ++)
+    for (int i = 0; i < 5; i++)
     {
         Branch* tree = new Branch("tree " + to_string(i + 1));
         trees.push_back(tree);
 
         int numBigBranch = rand() % 3 + 3;
-        for(int j = 0; j < numBigBranch; j ++)
+        for (int j = 0; j < numBigBranch; j++)
         {
-          Branch* bigBranch = new Branch("big branch " + to_string(j + 1));
-          tree->children.push_back(bigBranch);
+            Branch* bigBranch = new Branch("big branch " + to_string(j + 1));
+            tree->children.push_back(bigBranch);
 
-           int numMidBranch = rand() % 2 + 2;
-           for(int k = 0; k < numBigBranch; k ++)
-           {
-            Branch* midBranch = new Branch("midl branch " + to_string(k + 1));
-            bigBranch->children.push_back(midBranch);
-           }
-          
+            int numMidBranch = rand() % 2 + 2;
+            for (int k = 0; k < numMidBranch; k++)
+            {
+                Branch* midBranch = new Branch("midl branch " + to_string(k + 1));
+                bigBranch->children.push_back(midBranch);
+            }
         }
     }
 
     // Заселяем дома эльфами
-int count = 0;
-for (Branch* tree : trees) {
-    cout << "tree: " << tree->name << endl;
+    int count = 0;
+    for (Branch* tree : trees)
+    {
+        cout << "tree: " << tree->name << endl;
 
-    for (Branch* bigBranch : tree->children) {
-        cout << "big branch: " << bigBranch->name << "  ";
-        
+        for (Branch* bigBranch : tree->children)
+        {
+            cout << "big branch: " << bigBranch->name << "  ";
 
-        // Заселение на большие ветки
-        count++;
-        string elfName = "elf" + to_string(count);
-        cout << "the elf's name is: " << elfName << endl;
-
-        if (elfName != "None") {
-            Elf* elf = new Elf();
-            elf->name = elfName;
-            bigBranch->elves.push_back(elf);
-            
-        }
-
-        for (Branch* midBranch : bigBranch->children) {
-            cout << "midl branch: " << midBranch->name << " ";
-
-            // Заселение на средние ветки
+            // Заселение на большие ветки
             count++;
-            elfName = "elf" + to_string(count);
+            string elfName = "elf" + to_string(count);
             cout << "the elf's name is: " << elfName << endl;
 
-            if (elfName != "None") {
-                Elf* elf = new Elf();
-                elf->name = elfName;
-                midBranch->elves.push_back(elf);
+            if (elfName != "None")
+            {
+                Elf elf;
+                elf.name = elfName;
+                bigBranch->elves = elf;
             }
+
+            for (Branch* midBranch : bigBranch->children)
+            {
+                cout << "midl branch: " << midBranch->name << " ";
+
+                // Заселение на средние ветки
+                count++;
+                elfName = "elf" + to_string(count);
+                cout << "the elf's name is: " << elfName << endl;
+
+                if (elfName != "None")
+                {
+                    Elf elf;
+                    elf.name = elfName;
+                    midBranch->elves = elf;
+                }
+            }
+            cout << endl;
         }
         cout << endl;
     }
-    cout << endl;
-}
 
-   // Поиск эльфа по имени и подсчет соседей
-   string searchName;
-cout << "Enter elf's name to search: ";
-cin >> searchName;
+    // Поиск эльфа по имени и подсчет соседей
+    string searchName;
+    cout << "Enter elf's name to search: ";
+    cin >> searchName;
 
-int neighborCount = 0;
-for (Branch* tree : trees) {
-    for (Branch* bigBranch : tree->children) {
-      for (Elf* elf : bigBranch->elves) {
-          if (elf->name == searchName) {
-            neighborCount = bigBranch->children.size();
-            break;
-          }
-        for (Branch* midBranch : bigBranch->children) {
-            for (Elf* elf : midBranch->elves) {
-                if (elf->name == searchName) {
+    int neighborCount = 0;
+    for (Branch* tree : trees)
+    {
+        for (Branch* bigBranch : tree->children)
+        {
+            if (bigBranch->elves.name == searchName)
+            {
+                neighborCount = bigBranch->children.size();
+                break;
+            }
+            for (Branch* midBranch : bigBranch->children)
+            {
+                if (midBranch->elves.name == searchName)
+                {
                     // Подсчет соседей на средних ветках
-                    for (Branch* neighborMidBranch : bigBranch->children) {
-                        if (neighborMidBranch != midBranch) {
-                            neighborCount += neighborMidBranch->elves.size();
+                    for (Branch* neighborMidBranch : bigBranch->children)
+                    {
+                        if (neighborMidBranch != midBranch)
+                        {
+                            neighborCount += neighborMidBranch->elves.name != "" ? 1 : 0;
                         }
                     }
                     // Подсчет соседей на больших ветках
-                    for (Elf* elf : bigBranch->elves) {
-                        neighborCount++;
-                    }
+                    neighborCount += bigBranch->elves.name != "" ? 1 : 0;
                     break;
                 }
             }
         }
     }
-}
-}
-             
-     std::cout << "Total neighbors for " << searchName << ": " << neighborCount << std::endl;
-    
-     // Освобождаем память
-    for (Branch* tree : trees) {
-        for (Branch* bigBranch : tree->children) {
-            for (Branch* midBranch : bigBranch->children) {
-                for (Elf* elf : midBranch->elves) {
-                    delete elf;
-                }
-                delete midBranch;
-            }
+
+    cout << "Total neighbors for " << searchName << ": " << neighborCount << endl;
+
+    // Освобождаем память
+    for (Branch* tree : trees)
+    {
+        for (Branch* bigBranch : tree->children)
+        {
             delete bigBranch;
         }
         delete tree;
-        
-    }  
+    }
 
-    
+
     return 0;  
  
    
